6239f2ab8027500d19102e964f6de45111490f43
WANNES <wannes.vereecken.w1755@student.hogent.be>
2017-08-28 15:46:49 +0200

    update AvailabilityChecker

--- AvailabilityChecker.py
+++ AvailabilityChecker.py
@@ -8,14 +8,25 @@ class Book:
         self.ISBN = ISBN
 
         http = urllib3.PoolManager()
-        r = http.request('GET',
-                         'http://zoeken.bibliotheek.be/api/v0/search/?q=isbn:' + str(self.ISBN) + '&authorization=ac135e89f84460a251a6283a14180a22')
+        validatie = http.request('GET', 'http://zoeken.bibliotheek.be/validate.ashx?isbn=' + str(self.ISBN))
+        self.available = None
+        print(int(validatie.data))
+        if int(validatie.data) == 1:
+            r = http.request('GET',
+                             'http://zoeken.bibliotheek.be/api/v0/search/?q=isbn:' + str(
+                                 self.ISBN) + '&authorization=ac135e89f84460a251a6283a14180a22')
 
-        root = ET.fromstring(r.data)
+            root = ET.fromstring(r.data)
 
-        r = http.request('GET', 'http://zoeken.oost-vlaanderen.bibliotheek.be/api/v0/availability/?id=' + root[1][0][0].text + '&authorization=f2c359618130a698cca2e6b2736ab9fc&branch=Gent')
-        root = ET.fromstring(r.data)
-        self.available = root[1][0][0].attrib['available']
+            r = http.request('GET',
+                             'http://zoeken.oost-vlaanderen.bibliotheek.be/api/v0/availability/?id=' + root[1][0][
+                                 0].text + '&authorization=f2c359618130a698cca2e6b2736ab9fc&branch=Gent')
+            root = ET.fromstring(r.data)
+            self.available = root[1][0][0].attrib['available']
+        else:
+            print("ISBN is not valid")
+
+
 
 
 with open('Krookie.json') as data_file:
