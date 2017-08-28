e86d804f03ee804c32ebea0f7b17a6a95f797838
WANNES <wannes.vereecken.w1755@student.hogent.be>
2017-08-28 16:35:11 +0200

    update AvailabilityChecker

--- AvailabilityChecker.py
+++ AvailabilityChecker.py
@@ -5,42 +5,51 @@ from pprint import pprint
 
 class Book:
     def __init__(self, ISBN):
-        self.ISBN = ISBN
-
-        http = urllib3.PoolManager()
-        validatie = http.request('GET', 'http://zoeken.bibliotheek.be/validate.ashx?isbn=' + str(self.ISBN))
+        self.ISBN = str(ISBN)
         self.available = None
-        print(int(validatie.data))
+        self.check_availability()
+
+    def check_availability(self):
+        http = urllib3.PoolManager()
+        #Zoeken of het een geldig ISBN is, 1 = geldig 0 = niet geldig
+        validatie = http.request('GET', 'http://zoeken.bibliotheek.be/validate.ashx?isbn=' + self.ISBN)
+
         if int(validatie.data) == 1:
+            #Id van het boek zoeken
             r = http.request('GET',
                              'http://zoeken.bibliotheek.be/api/v0/search/?q=isbn:' + str(
                                  self.ISBN) + '&authorization=ac135e89f84460a251a6283a14180a22')
 
             root = ET.fromstring(r.data)
-
+            id = root[1][0][0].text
+            
+            #Availability van het boek zoeken
             r = http.request('GET',
-                             'http://zoeken.oost-vlaanderen.bibliotheek.be/api/v0/availability/?id=' + root[1][0][
-                                 0].text + '&authorization=f2c359618130a698cca2e6b2736ab9fc&branch=Gent')
+                             'http://zoeken.oost-vlaanderen.bibliotheek.be/api/v0/availability/?id=' + id + '&authorization=f2c359618130a698cca2e6b2736ab9fc&branch=Gent')
+
             root = ET.fromstring(r.data)
             self.available = root[1][0][0].attrib['available']
+
         else:
             print("ISBN is not valid")
 
 
+#Main
 
-
+#Lees JSON file
 with open('Krookie.json') as data_file:
     data = json.load(data_file)
+
+#Filter enkel de boeken eruit
 books_data = data['books']
 
+#Maak book objecten voor elk boek
 books = [Book(book_data["ISBN"]) for book_data in books_data]
 
+#Toevoegen/Aanpassen available attribuut
 for i, book_data in enumerate(data['books']):
-
     book_data['available'] = books[i].available
 
+#Updaten JSON bestand
 with open('Krookie.json', 'w') as outfile:
     json.dump(data, outfile, indent= 2)
-
-
-
